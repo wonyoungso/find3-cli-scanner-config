d234e81ad99b452027385d7c2f08f7c7bc644835
Wonyoung So <wso@mit.edu>
2019-04-27 01:15:57 -0400

    update

---
+++ find3-cli-scanner-daemon.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+echo "Hello wolrd!"
+# while [ 1 ]; do
+#     echo "Hello World!"
+#     sleep 1
+# done
+# sudo ifconfig wlan0 down
+# sudo ifconfig wlan1 down
+# sudo iwconfig wlan0 mode monitor
+# sudo iwconfig wlan1 mode monitor
+# sudo ifconfig wlan0 up
+# sudo ifconfig wlan1 up
+
+#sh
