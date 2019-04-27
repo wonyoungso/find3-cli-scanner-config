5469da0fc9f1d88d426bc5848db95e0dc444cf6a
wso-pi-yuchen <wso-pi-yuchen@mit.edu>
2019-04-26 21:03:45 -0900

    udpate

--- find3-cli-scanner-daemon.sh
+++ find3-cli-scanner-daemon.sh
@@ -1,15 +1,32 @@
 #!/bin/sh
 
-echo "Hello wolrd!"
-# while [ 1 ]; do
-#     echo "Hello World!"
-#     sleep 1
-# done
-# sudo ifconfig wlan0 down
-# sudo ifconfig wlan1 down
-# sudo iwconfig wlan0 mode monitor
-# sudo iwconfig wlan1 mode monitor
-# sudo ifconfig wlan0 up
-# sudo ifconfig wlan1 up
+deviceName="yuchen"
+
+echo "turn on bluetooth"
+systemctl start bluetooth
+
+echo "System Started"
+ifconfig wlan0 down
+ifconfig wlan1 down
+echo "All Wifi is down"
+iwconfig wlan0 mode monitor | grep failed
+if ($? -eq 0); then
+	monitorID="wlan1"
+else
+	monitorID="wlan0"
+fi
+iwconfig wlan1 mode monitor
+ifconfig wlan0 up
+ifconfig wlan1 up
+
+echo "monitorID is: $monitorID"
+
+docker stop scanner
+docker rm scanner
+docker run --net="host" --privileged --name scanner -d -i -t schollz/find3-cli-scanner 
+
+cmd="docker exec scanner find3-cli-scanner -wifi -bluetooth -server https://crowdsourcedcity-api.herokuapp.com -i ${monitorID} -passive -scantime 10 -forever -no-modify -device ${deviceName} -family family"
+
+eval "$cmd"
 
 #sh
